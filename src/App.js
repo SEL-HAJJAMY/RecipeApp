c0c497e2617aaca1c925d3c31a772f3df1f80e19
SAFAE <mac@MacBook-Pro-de-mac.local>
2022-10-24 22:02:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {YOUR_APP_ID, YOUR_APP_KEY } from './key';
+import axios from 'axios';
+
 
 function App() {
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+
+async function getRecipe(){
+
+  var result = await axios.get(url);
+  console.log(result.data);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 onClick={getRecipe}>Hello world</h1>
+
     </div>
   );
 }
 
 export default App;
+
+
+
+// Notes: 
+// - Axios : is a promise based HTTP request library which allows us to interface with REST API. 
+//           More thing that we can cancel request and intercept request.
