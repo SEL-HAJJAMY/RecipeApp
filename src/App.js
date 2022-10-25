17bcee3ede95962e66e1c0b79e981ba892846036
SAFAE <mac@MacBook-Pro-de-mac.local>
2022-10-25 10:28:42 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -2,21 +2,72 @@
 import './App.css';
 import {YOUR_APP_ID, YOUR_APP_KEY } from './key';
 import axios from 'axios';
+import { useState } from 'react';
+import RecipeTitle from './RecipeTitle';
 
 
 function App() {
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+  const [query,  setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  // const [healthLabels, setHealthLabels] = useState("");
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
 
 async function getRecipe(){
 
   var result = await axios.get(url);
-  console.log(result.data);
+  setRecipes(result.data.hits);
+  console.log(result.data.hits);
+}
+
+const onSubmit = (e) =>{
+  e.preventDefault();
+  getRecipe();
 }
 
 
   return (
-    <div className="App">
-     <h1 onClick={getRecipe}>Hello world</h1>
+    <div className="app">
+     <h1 onClick={getRecipe}>Our Food Recipes</h1>
+     <form className='app__searchForm' onSubmit={onSubmit}>
+      <input
+      className='app__input' 
+      type="text" 
+      placeholder='Search recipe'
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}/>
+
+      <input className='app__submit' type='submit' value = 'Search' />
+      {/* <select className='app__healthLabel'>
+        <option onClick={() => setHealthLabels("vegan")}>Vegan</option>
+        <option onClick={() => setHealthLabels("vegetarian")}>Vegetarian</option>
+        <option onClick={() => setHealthLabels("paleo")}>Paleo</option>
+        <option onClick={() => setHealthLabels("dairy-free")}>Dairy-free</option>
+        <option onClick={() => setHealthLabels("wheat-free")}>Wheat-free</option>
+        <option onClick={() => setHealthLabels("low-sugar")}>Low-sugar</option>
+        <option onClick={() => setHealthLabels("egg-free")}>Egg-free</option>
+        <option onClick={() => setHealthLabels("peanut-free")}>Peanut-free</option>
+        <option onClick={() => setHealthLabels("tree-nut-free")}>Tree-nut-free</option>
+        <option onClick={() => setHealthLabels("soy-free")}>Soy-free</option>
+        <option onClick={() => setHealthLabels("fish-free")}>Fish-free</option>
+        <option onClick={() => setHealthLabels("shellfish-free")}>Shellfish-free</option>
+
+   
+     
+     
+     
+      </select> */}
+     </form>
+
+
+    <div className='app__recipes'>
+      {recipes.map((recipe) => {
+        return <RecipeTitle recipe={recipe} />;
+      })}
+
+     </div>
+     
+
+
 
     </div>
   );
